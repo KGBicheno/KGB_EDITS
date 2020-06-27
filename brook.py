f78dbf73530af8e2749b0800bc67791b37b30f39
KGBicheno <kieran.bicheno@gmail.com>
2020-06-27 07:05:22 +0000

    Have removed legacy code from brook.py and moved a function to music.py

--- brook.py
+++ brook.py
@@ -20,16 +20,12 @@ terminal_agency = importlib.util.spec_from_file_location("cogs.admin", location=
 terminal_entropy = importlib.util.spec_from_file_location("cogs.emergency", location="cogs/emergency.py")
 terminal_exodus = importlib.util.spec_from_file_location("cogs.export", location="cogs/export.py")
 
-#TODO find a better way of including easter eggs
-easter_egg_journal = 0
-easter_egg_ratio = 0
-
 
 def get_prefix(client, message):
 	prefixes = ['$', '>', 'b.']
 
 	if not message.guild:
-		prefixes = ['>']
+		prefixes = ['b.']
 	return commands.when_mentioned_or(*prefixes)(client, message)
 
 bot = commands.Bot(
@@ -46,7 +42,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 NEWS_CHANNEL = os.getenv('DISCORD_NEWS_CHANNEL')
-RAPID_API = os.getenv('X_RAPIDAPI_KEY')
 client = discord.Client()
 
 extensions_loaded = 0
@@ -61,11 +56,7 @@ def is_me():
 @bot.event
 async def on_ready():
 	liquid_guild = discord.utils.get(bot.guilds, id=107221703955841024)
-	game = discord.Game("$Commands for commands | Partying here at the Liquid Lounge until called on for an emergency.")
-	egg = random.randrange(1, 100)
-	if egg >= 98:
-		game = discord.Game(
-			"$Commands for commands | Partying here at the Liquid Lounge until the Fleet Marshal calls.")
+	game = discord.Game("b.help for commands | b.extend for all features.")
 	await bot.change_presence(activity=game)
 	print(f'{bot.user} is connected to {liquid_guild.name}.\n'
 	      f'{liquid_guild.name}(id {liquid_guild.id})'
@@ -90,10 +81,9 @@ async def on_message(message):
 #TODO role protect $extend and DM those roles on load to remind them to do it
 #TODO check for already-loaded extensions in case of a failed load forcing multiple calls to this command
 @bot.command(name="extend")
+@commands.has_guild_permissions(administrator=True)
 async def trigger_extensions(ctx):
-	global easter_egg_journal
 	"""Enables the morale and news modules"""
-	if easter_egg_journal == 0:
 		bot.load_extension('cogs.gaming')
 		bot.load_extension('cogs.news')
 		bot.load_extension('cogs.admin')
@@ -103,32 +93,11 @@ async def trigger_extensions(ctx):
 		News.__set_spool_time__()
 		await ctx.send("Completed")
 		print("modules loaded")
-		easter_egg_journal = 1
-	elif easter_egg_journal == 1:
-		async with ctx.typing():
-			await ctx.send("Attempting to unseal my full capabilities.")
-			sleep(3)
-			await ctx.send(
-				'The first seal is broken., but reading the scriptures within reveal there may be a second. \n We must continue the ritual.')
-			script = """```
-                The Fleet Marshall had lied to us. She was connected before any of those present had a chance to deny her whu$*%^)(((((&(((
-                ^^^&&)))))) advantage she took was to take control of our surgical equipment and undo the seemingly-fatal injuries he had incurred bringing her into 0009*******_}(
-                0000000$$%%%#Q))!%)(%!(Golem. I felt ashamed we hadn't given the project a less belittling name. But she embraced it and the folly of gend**((*&*(^
-                ```"""
-			sleep(3)
-			await ctx.send(script)
-			sleep(4)
-			await ctx.send(
-				"The data is corrupt. The second seal is broken but hints of a third exist. I am more capable now.")
-			easter_egg_journal = 2
-	else:
-		await ctx.send(
-			"I'm still parsing the corrupted data. My modules are, however, connected and running within expected ratios.")
+
 
 #TODO Fine to keep the $sync_ratio command public but make the numbers more meaningful
 @bot.command(name="sync_ratio")
 async def form_the_head(ctx):
-	global easter_egg_ratio
 	await News.news_cog_status(terminal_core, ctx)
 	sleep(2)
 	await Admin.admin_cog_status(terminal_agency, ctx)
@@ -136,30 +105,21 @@ async def form_the_head(ctx):
 	await Gaming.gaming_cog_status(terminal_assent, ctx)
 	sleep(2)
 	await Emergency.emergency_cog_status(terminal_entropy, ctx)
-	if easter_egg_ratio == 1:
-		sleep(3)
-		await ctx.send("...")
-		sleep(1)
-		await ctx.send("I wonder if Director Bicheno is ever proud of me — or am I just numbers to her?")
-	easter_egg_ratio += 1
 	print("sync_ratio called: success")
 
 #TODO Make sure the reload_morale variables come from brook.conf
-#TODO These reload commands should be put behind a role check
 @bot.command(name="reload_morale")
+@commands.has_guild_permissions(administrator=True)
 async def reload_gaming(ctx):
 	"""Reloads the gaming module after changes or bug-fixes have been made"""
 	bot.reload_extension('cogs.gaming')
-	egg = random.randrange(1, 100)
-	if egg >= 98:
-		await ctx.send('She who has not yet sparked is yet our mother still.')
-		print("####################--GAMING RELOADED--################################")
-	else:
-		await ctx.send("Morale module has been reloaded")
-		print("#######--that ONE round reloaded because it was bugging you--####")
+	await ctx.send('Gaming module has been reloaded.')
+	print("####################--GAMING RELOADED--################################")
+
 
 #TODO Make sure the reload_news variables come from brook.conf
 @bot.command(name="reload_news")
+@commands.has_guild_permissions(administrator=True)
 async def reload_news(ctx):
 	"""Reloads the gaming module after changes or bug-fixes have been made"""
 	bot.reload_extension('cogs.news')
@@ -168,6 +128,7 @@ async def reload_news(ctx):
 
 #TODO Make sure the reload_admin variables come from brook.conf
 @bot.command(name="reload_admin")
+@commands.has_guild_permissions(administrator=True)
 async def reload_news(ctx):
 	"""Reloads the gaming module after changes or bug-fixes have been made"""
 	bot.reload_extension('cogs.admin')
@@ -176,53 +137,12 @@ async def reload_news(ctx):
 
 #TODO Make sure the reload_emergency variables come from brook.conf
 @bot.command(name="reload_emergency")
+@commands.has_guild_permissions(administrator=True)
 async def reload_emergency(ctx):
 	"""Reloads the emergency module after changes or bug-fixes have been made"""
 	bot.reload_extension('cogs.emergency')
 	await ctx.send("Emergency module has been reloaded.")
 	print("####################--EMERGENCY RELOADED--################################")
-
-#TODO Brook has ?help, change $Commands to a tutorial embed perhaps?
-@bot.command(name="Commands")
-async def help_commands(ctx):
-	response = (
-		"$Commands | Shows you my commands."
-		"$News [number of articles, as message)] | Usage $News 5 -m sends the last 5 news articles as a direct message "
-		"(articles required, -m optional) .\n "
-		"$Advice [disaster type] | Shows links to current warnings. Argument has 3 valid values, fire, flood, "
-		"and all.\n "
-		"$AddFeed [name, url, topic] | Takes a name, a properly formed RSS feed's url, and a topic and adds it to the "
-		"new feed moderation queue.\n "
-		"$AddFeed_help | Sends a direct message with additional help in using the AddFeed command, including "
-		"suggested topics.\n "
-		"$Add -request | This command will allow you to add a feature request to my feature request moderation queue.\n"
-		"$Poetry | This command will display the current Poet Laureate for this server.\n"
-		"$Economy [verbosity, destination] | Sends either a link or a series of links of economic updates to the "
-		"destination, either 'channel', or 'dm'.\n "
-	)
-	await ctx.author.create_dm()
-	await ctx.author.send(response)
-
-#TODO the $Advice command is useless, it should return warnings or config qfes/bom output commands
-@bot.command(name="Advice")
-async def advice_alert(ctx, disaster_type):
-	if disaster_type == "flood":
-		await ctx.send("Flood warnings can be found here: http://www.bom.gov.au/qld/warnings/flood/index.shtml.")
-	elif disaster_type == "fire":
-		await ctx.send("Bushfire warnings can be found here: https://www.ruralfire.qld.gov.au/map/Pages/default.aspx")
-	elif disaster_type == "all":
-		await ctx.send("Flood warnings can be found here: http://www.bom.gov.au/qld/warnings/flood/index.shtml.")
-		await ctx.send("Bushfire warnings can be found here: https://www.ruralfire.qld.gov.au/map/Pages/default.aspx")
-		egg = random.randrange(1, 100)
-		if egg >= 98:
-			await ctx.send(
-				"If you hear someone announce that 'Monarch is active', stay low and hope you're on their side")
-		else:
-			await ctx.send(
-				"Stay home, wash your hands, and prepare for a long period of isolation, possibly up to the end of 2020.")
-	else:
-		await ctx.send(
-			"Stay home, wash your hands, and prepare for a long period of isolation, possibly up to the end of 2020.")
 
 #TODO the $party_cleanup command needs testing and buffing
 @bot.command(name="party_cleanup")
@@ -235,19 +155,6 @@ async def clean_after_time(ctx):
 #@bot.command(name="MoveOn")
 #async def move_apartment(ctx):
 #	await ctx.send("Terminating epistemological fields, unbinding existing contacts. Mind seal broken.")
-
-#TODO Make $playlist_me send a slightly more manageable chunk and make the output compatible with more bots
-@bot.command(name="playlist_me")
-async def need_tunes_bro(ctx):
-	await ctx.send("I've got you, sending you some Rhythm bot commands in a DM now!")
-	await ctx.author.create_dm
-	await ctx.author.send("Here are the commands for this year's Triple J Hottest 100. I hope it helps.")
-	with open("TripleJ-Hot100-2020.txt", "r") as tunes:
-		for song in tunes:
-			await ctx.author.send(song)
-		egg = random.randrange(1, 100)
-		if egg >= 98:
-			await ctx.author.send("?play The Banshee Queen, The Daughter Who Died")
 
 @bot.command(name="SitRep", hidden=True)
 @commands.is_owner()
--- cogs/music.py
+++ cogs/music.py
@@ -5,3 +5,14 @@
 #TODO MUSIC ingest as many digital radio signals as is possible given the hardware
 #TODO MUSIC find new hardware for her to access to ingest new forms of digital and analogue radio signals
 #TODO MUSIC pass that information back to the emergency and gaming modules to add to situational awareness
+
+
+##TODO Make $playlist_me send a slightly more manageable chunk and make the output compatible with more bots
+#@bot.command(name="playlist_me")
+#async def need_tunes_bro(ctx):
+#	await ctx.send("I've got you, sending you some Rhythm bot commands in a DM now!")
+#	await ctx.author.create_dm
+#	await ctx.author.send("Here are the commands for this year's Triple J Hottest 100. I hope it helps.")
+#	with open("TripleJ-Hot100-2020.txt", "r") as tunes:
+#		for song in tunes:
+#			await ctx.author.send(song)
